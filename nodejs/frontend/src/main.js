e133749923728d4755712dcb31826f439eae1d9a
Nicolas Amadori - nicolas.amadori@studio.unibo.it <nicolas.amadori@studio.unibo.it>
2025-05-19 15:45:44 +0200

    first implementation of homepage

--- nodejs/frontend/src/main.js
+++ nodejs/frontend/src/main.js
@@ -2,12 +2,14 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 
-import BootstrapVue3 from 'bootstrap-vue-3'
+import {createBootstrap} from 'bootstrap-vue-next'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+import VueDatePicker from '@vuepic/vue-datepicker';
 
 createApp(App)
     .use(router)
-    .use(BootstrapVue3)
+    .use(createBootstrap())
+    .component('VueDatePicker', VueDatePicker)
     .mount('#app')
