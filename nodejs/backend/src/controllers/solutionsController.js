b913ed674ecd1e9e0ea13123c43462c93f14ae6b
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2025-05-29 10:40:33 +0200

    Now insert many solutions work

--- nodejs/backend/src/controllers/solutionsController.js
+++ nodejs/backend/src/controllers/solutionsController.js
@@ -24,30 +24,35 @@ export const get_solutions = async function(req, res) {
         });
 
         const data = await response.json();
-        // await Solution.insertMany(data.solutions.array.map(solution => ({
-        //     solution_id: solution.id,
-        //     origin: solution.origin,
-        //     destination: solution.destination,
-        //     departure_time: new Date(solution.departureTime),
-        //     arrival_time: new Date(solution.arrivalTime),
-        //     duration: solution.duration,
-        //     status: solution.status,
-        //     price_currency: solution.price.currency,
-        //     price_amount: solution.price.amount,
-        //     nodes: solution.nodes.map(node => ({
-        //         origin: node.origin,
-        //         destination: node.destination,
-        //         departure_time: new Date(node.departureTime),
-        //         arrival_time: new Date(node.arrivalTime),
-        //         train: {
-        //             denomination: node.train.denomination,
-        //             acronym: node.train.acronym,
-        //             name: node.train.name
-        //         }
-        //     }))
-        // })));
+
+        try {
+            await Solution.insertMany(data.solutions.map(solution => ({
+                solution_id: solution.solution.origin + "|" + solution.solution.destination + "|" + (new Date(solution.solution.departureTime).toISOString()) + "|" + (new Date(solution.solution.arrivalTime).toISOString()),
+                origin: solution.solution.origin,
+                destination: solution.solution.destination,
+                departure_time: new Date(solution.solution.departureTime),
+                arrival_time: new Date(solution.solution.arrivalTime),
+                duration: solution.solution.duration,
+                status: solution.solution.status,
+                price_currency: solution.solution.price?.currency,
+                price_amount: solution.solution.price?.amount,
+                nodes: solution.solution.nodes?.map(node => ({
+                    origin: node.origin,
+                    destination: node.destination,
+                    departure_time: new Date(node.departureTime),
+                    arrival_time: new Date(node.arrivalTime),
+                    train: {
+                        denomination: node.train?.denomination,
+                        acronym: node.train?.acronym,
+                        name: node.train?.name
+                    }
+                }))
+            })), { ordered: false });
+        } catch (e) {
+            // console.warn("Duplicates skipped:", e.writeErrors);
+        }
         return res.status(200).json(data);
     } catch (err) {
         return res.status(500).json({ message: err.message });
     }
-  };
+};
--- nodejs/backend/src/models/solutionModel.js
+++ nodejs/backend/src/models/solutionModel.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose';
 var Schema = mongoose.Schema;
 
 var SolutionSchema = new Schema({
-    solution_id: { type: String, required: true, },
+    solution_id: { type: String, required: true, unique: true },
     origin: { type: String, required: true, },
     destination: { type: String, required: true },
     departure_time: { type: Date, required: true },
     arrival_time: { type: Date, required: true },
-    duration: { type: String, required: false },
-    status: { type: String, required: false },
+    duration: { type: String, required: true },
+    status: { type: String, required: true },
     price_currency: { type: String, required: false },
     price_amount: { type: Number, required: false },
     nodes: [{
@@ -17,9 +17,9 @@ var SolutionSchema = new Schema({
         departure_time: { type: Date, required: true },
         arrival_time: { type: Date, required: true },
         train: {
-            denomination: { type: String, required: false },
-            acronym: { type: String, required: false },
-            name: { type: String, required: false }
+            denomination: { type: String, required: true },
+            acronym: { type: String, required: true },
+            name: { type: String, required: true }
         }
     }]
 });
